f2ac38a1d635b496068c377ee0e9f13c007e167f
wanghep <53277985@qq.com>
2018-05-30 14:47:41 +0800

    update

--- LSlamSDK/Assets/CameraSelect.cs
+++ LSlamSDK/Assets/CameraSelect.cs
@@ -23,13 +23,11 @@ public class CameraSelect : MonoBehaviour {
 
     IPoseListener m_poseListener;
 
-    public DeviceIndex source;
-
-    public bool UsePosition = true;
-    public bool UseRotation = true;
     public Confidence MinConfidence = Confidence.LOW;
+    //slam数据优化
     public float predictionTime = 0;
     Intel.RealSense.Tracking.Pose m_pose;
+    //相关关联的camera 位置
     public Transform Mycamera;
 
 
@@ -60,10 +58,8 @@ public class CameraSelect : MonoBehaviour {
         while (true)
         {
             yield return waitConnect;
-            Debug.LogFormat(this, "Device {0} ready, Waiting for {1} poses", tm2.device, source);
-
-            if (source == DeviceIndex.HMD)
-            {
+           
+             {
                 m_poseListener = tm2.device;
 
 #if UNITY_2017_1_OR_NEWER
@@ -73,48 +69,11 @@ public class CameraSelect : MonoBehaviour {
 #endif
 
             }
-            else
-            {
-                //					yield return new WaitUntil (tm.device.IsControllerConnected((int)source));
-
-                tm2.device.onControllerDiscovery += (IControllerDevice ctrl) => {
-                    Debug.Log("onControllerDiscovery: " + ctrl);
-
-                    var info = ctrl.Info;
-                    Debug.LogFormat("VendorData: {0}", info.VendorData);
-
-                    ctrl.onConnect += (int index) => {
-                        Debug.Log("onConnect: " + ctrl);
-                        Debug.LogFormat("id={0}, source={1}", index, source);
-
-                        if (index != (int)source)
-                            return;
-
-                        m_poseListener = ctrl;
-
-                        ctrl.onDisconnect += (int index2) => {
-                            if (index2 != (int)source)
-                                return;
-                            Debug.LogFormat("onDisconnect: index={0}", index2);
-                            m_poseListener = null;
-                        };
-                    };
-                };
-
-                yield return waitConnectCtrl;
-
-                if (m_poseListener == null)
-                {
-                    Debug.LogFormat("device disconnected while waiting for controller {0}", source);
-                    continue;
-                }
-
-                Debug.LogFormat("WaitForPose: {0}, source={1}", m_poseListener, source);
-            }
+            
 
             yield return waitDisconnect;
 
-            Debug.LogFormat("{0} ({1}) disconnected", m_poseListener, source);
+            Debug.LogFormat("{0}  disconnected", m_poseListener);
 
 #if UNITY_2017_1_OR_NEWER
             Application.onBeforeRender -= Update;
@@ -126,9 +85,92 @@ public class CameraSelect : MonoBehaviour {
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnDestroy()
+    {
+        m_poseListener = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_poseListener == null)
+            return;
+        UpdatePose();
+    }
+
+    void onPreCull(Camera cam)
+    {
+        Update();
+    }
+
+    static readonly Quaternion q = Quaternion.Euler(-90, 180, 0);
+
+    void UpdatePose()
+    {
+        if (!m_poseListener.TryGetPose(ref m_pose))
+            return;
+
+        if (m_pose.trackerConfidence < MinConfidence)
+            return;
+
+        if (predictionTime > 0)
+            PredictPose(ref m_pose, predictionTime);
+
+        var pos = m_pose.translation;
+        var rot = m_pose.rotation;
+
+        rot.x = -rot.x;
+        rot.w = -rot.w;
+        rot = q * rot;
+
+        pos.Set(pos.x, pos.z, pos.y);
+
+        m_transform.localPosition = pos;
+        m_transform.localRotation = rot;
+
+        Mycamera.position = pos;
+        Mycamera.rotation = rot;
+
+
+
+
+    }
+
+    private void PredictPose(ref Intel.RealSense.Tracking.Pose pose, float dt)
+    {
+
+        // yinon: limit prediction to 100ms ahead
+        if (dt > 0.1f)
+            dt = 0.1f;
+        // "aliasing"
+        Vector3 T = pose.translation;
+        Vector3 vw = pose.angularVelocity;
+        Vector3 vt = pose.velocity;
+        Vector3 aw = pose.angularAcceleration;
+        Vector3 at = pose.acceleration;
+
+        pose.translation.x = dt * (dt / 2.0f * at.x + vt.x) + T.x;
+        pose.translation.y = dt * (dt / 2.0f * at.y + vt.y) + T.y;
+        pose.translation.z = dt * (dt / 2.0f * at.z + vt.z) + T.z;
+
+        Vector3 W = new Vector3(dt * (dt / 2.0f * aw.x + vw.x),
+                                 dt * (dt / 2.0f * aw.y + vw.y),
+                                 dt * (dt / 2.0f * aw.z + vw.z));
+
+        Quaternion quatW = quaternionExp(W);
+
+        pose.rotation = (quatW * pose.rotation);
+    }
+
+    Quaternion quaternionExp(Vector3 v)
+    {
+        Vector3 w = new Vector3(v.x / 2.0f, v.y / 2.0f, v.z / 2.0f);
+        float th2 = w.x * w.x + w.y * w.y + w.z * w.z;
+        float th = Mathf.Sqrt(th2);
+        float c = Mathf.Cos(th);
+        float s = th2 < Mathf.Sqrt(120 * Mathf.Epsilon) ? 1.0f - 1.0f / 6.0f * th2 : Mathf.Sin(th) / th;
+        Quaternion Q = new Quaternion(s * w.x, s * w.y, s * w.z, c);
+        return Q;
+    }
 }
